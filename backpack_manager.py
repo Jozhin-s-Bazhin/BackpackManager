c247f1ba1ee66a1798aa0b6906ec492ca9fb519b
roman <rbezroutchko@gmail.com>
2023-11-04 23:43:11 +0100

    Second earliest version. Fully done but not working due to a bug.

--- backpack_manager.py
+++ backpack_manager.py
@@ -1,7 +1,26 @@
+### BACKPACK MANAGER ###
+
+# Tired of carrying unnecessary weight? Let BackpackManager™ take the job and make the perfect configuration of folders to minimise your back pain!
+
+
+# Data structure: There are 4 classes, Subject() which represents one subject you need to carry and has a name and weight, Day() which represents a day with a list of subjects you need to bring that day,
+# Folder() which represents a folder you carry one or more subjects in with its own weight and the weight of all the subjects in it, Configuration() which represents a certain combination of folders that should achieve a minimal average weight with a list of folders
+
+
+
+
+# Initialising the classes and variables used for the optimisation algorithm
+
 import random
 
+FOLDER_WEIGHT = 50
+MAX_FOLDERS_PER_DAY = 4
+MAX_SUBJECTS_PER_FOLDER = 4
+INITIAL_POPULATION = 1000
+
+
 class Subject():
-    """This class represents a subject. A subject has a name and weight."""
+    """This class represents a subject - has a name and weight."""
     subject_list = []
 
     def __init__(self, name, weight):
@@ -31,7 +50,7 @@ Du = Subject("Du", 30)
 
 
 class Day():
-    """This class represents a day. A day has 7 subjects except Wednesday (4)."""
+    """This class represents a day - contains 7 subjects except Wednesday (4)."""
 
     schedule_list = []
 
@@ -39,176 +58,110 @@ class Day():
         self.subjects = subjects
         Day.schedule_list.append(self)
 
+    def __repr__(self):
+        return (self.schedule_list)
+
 
 Monday = Day([Nd, Ch, Fr, En, Wi, Ge, Fr])
 Tuesday = Day([Fy, Av, Bi, Gd, LO, Aa, MEV])
-Wednesday = Day([St, Du, Nd, Ch, Fr, En, Wi])
+Wednesday = Day([St, Du, Nd, Ch])
 Thursday = Day([Ge, Fy, Av, Bi, Gd, LO, Aa])
 Friday = Day([MEV, St, Du, Nd, Ch, Fr, En])
 
 
 class Folder():
-    """This class represents a folder. A folder can have up to 4 subjects in it."""
+    """This class represents a folder - contains up to 4 subjects in it."""
 
-    def __init__(self, subjects,):
+    def __init__(self, subjects):
         self.subjects = subjects
         self.total_weight = 0
+        self.is_folder_valid = False
 
+        # Calculates if a folder has a valid amount of subjects
+        if len(self.subjects) > MAX_SUBJECTS_PER_FOLDER and len(self.subjects) >= 1:
+            self.is_folder_valid = True
+
+        # Calculates the total weight of a folder
         for subject in subjects:
             self.total_weight += subject.weight
-        self.total_weight += 50
+        self.total_weight += FOLDER_WEIGHT
 
     def __str__(self):
         return str(self.subjects)
 
 
 class Configuration():
-    """This class represents a configuration"""
+    """This class represents a configuration - contains a list of folders"""
 
     def __init__(self, folders):
         self.folders = folders
-        folder_sum_weight = 0
-        folder_sum_total = []
+
+        # Makes a list of all folders used each day
+        self.total_weight = 0
+        self.is_configuration_valid = True
 
         for day in Day.schedule_list:
-            folder_sum_per_day = []
-
+            folders_in_day = []
             for subject in day.subjects:
-                for folder in folders:
-                    if subject in folder:
-                        if folder not in folder_sum_per_day:
-                            folder_sum_per_day.append(folder)
 
-            for folder in folder_sum_per_day:
-                folder_sum_weight += folder.total_weight
+                for folder in self.folders:
+                    if subject in folder.subjects:
+                        if folder not in folders_in_day:
+                            folders_in_day.append(folder)
 
-            folder_sum_total.append(folder_sum_per_day)
+            # Calculates weight per week
+            for folder in folders_in_day:
+                self.total_weight += folder.total_weight
 
-        average_weight = folder_sum_weight / len(folder_sum_total)
+            # Calculates if configuration is valid
+            if len(folders_in_day) > MAX_FOLDERS_PER_DAY:
+                self.is_configuration_valid = False
 
     def __repr__(self):
-        return " ".join(map(str, self.folders))
-
-    average_weight = "Initialisation went wrong"
+        return (str("Folders: {}, Valid: {}, Total weight: {}".format(self.folders, self.is_configuration_valid, self.total_weight)))
 
 
-MAX_FOLDER_SIZE = 4
-population = []
+# Test for Configuration() class, self.folders, self.is_configuration_valid and self.total_weight parameters
 
+# test_config = Configuration([[Aa, Nd], [Ch, Fy], [Av, Bi, Gd, LO], [MEV, St, Du], [Fr], [En], [Wi]])
+# print(test_config)
+
+
+
+# Creating initial population
 
 def mk_random_folder(subject_list):
-    folder_size = random.randint(1, MAX_FOLDER_SIZE)
+    while True:
+        subjects_in_folder = []
+        for i in range(random.randint(1, MAX_SUBJECTS_PER_FOLDER)):
+            if len(subject_list) > 0:
+                subjects_in_folder.append(subject_list[0])
 
-    folder_contents = []
-    for i in range(1, folder_size):
-        if len(subject_list) > 0:
-            folder_contents.append(subject_list[0])
-            subject_list.remove(subject_list[0])
+def mk_random_configuration(subject_list):
+    while True:
+        folder_list = []
+        while True: 
+            subject_list_random = random.sample(subject_list)
+            new_folder = mk_random_folder(subject_list_random)
+            subject_list_random = [subject for subject in subject_list_random if subject not in new_folder]
 
-    return Folder(folder_contents)
+            folder_list.append(new_folder)
+            if subject_list_random <= 0:
+                break
+        if Configuration(folder_list).is_configuration_valid:
+            return Configuration(folder_list)
 
-
-def is_config_valid(new_config):
-    success = False
-
-    for day in Day.schedule_list:
-        folders_in_day = []
-
-        for subject in day.subjects:
-            # 'subject' will be studied in 'day'
-            for folder in new_config.folders:
-                if subject in folder.subjects:
-                    if folder not in folders_in_day:
-                        folders_in_day.append(folder)
-
-        if len(folders_in_day) > 4:
-            success = False
-            break
-        else:
-            success = True
-    return success
-
-
-def mk_new_random_config(subject_list):
-    subject_list_temp = list(subject_list)
-    random.shuffle(subject_list_temp)
-
-    new_config = Configuration([])
-    while subject_list_temp:
-        folder = mk_random_folder(subject_list_temp)
-        new_config.folders.append(folder)
-    return new_config
-
-
-def add_random_configuration():
-    folder_list = []
-    success = False
-
-    while success == False:
-        new_config = mk_new_random_config(Subject.subject_list)
-        success = is_config_valid(new_config)
-
-        if success == True:
-            # print(f"success is True {new_config}")
-            for folder in new_config.folders:
-                if folder == []:
-                    new_config.folders.remove(folder)
-            population.append(new_config)
-        # else:
-            # print(f"success is False {new_config}")
-
-
-for i in range(1, 10):
-    add_random_configuration()
-print(population)
-
-# def test_mk_random_folder():
-#     subject_list = list(Subject.subject_list)
-#     print(f"subject_list before {subject_list}")
-#     random_folder = mk_random_folder(subject_list)
-#     print(f"random_folder {random_folder}")
-#     print(f"subject_list after {subject_list}")
-
-
-# def test_is_config_valid():
-#     subject_list = list(Subject.subject_list)
-#     print(f"subject_list before {subject_list}")
-
-#     random_config = mk_new_random_config(subject_list)
-#     valid = is_config_valid(random_config)
-
-#     print(f"subject_list after {subject_list}")
-#     print(f"random_config {random_config}")
-#     print(f"valid {valid}")
-
-
-# test_is_config_valid()
-
-# def mutate1(c):
-#     while True:
-#         op = random swap? move? move_to_new?
-
-#         # swap
-#         d1 = random
-#         d2 = random
-#         f1 = random
-#         f2 = random
-#         if f1 == f2:
-#             continue
-
-#         s1 = random
-#         s2 = random
-#         if s1 == s2:
-#             continue
-
-#         xxxx
-#         validate
-#     return cc
-
-# for _ in range(100)
-
-
-
-# while True:
+population = []
+for i in range(INITIAL_POPULATION+1):
+    new_configuration = mk_random_configuration(Subject.subject_list)
+    population.append(new_configuration)
 
 print(population)
+
+
+
+# Genetic algorythm used to get the most optimal answer
+
+while True:
+    for i in range(1, 1000+1):
+        pass
